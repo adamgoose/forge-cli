02e3916f53651e0d34b9e0bfe94a76445b00c260
Adam Engebretson <adam@enge.me>
2014-07-25 16:16:06 -0500

    Adding Monitoring Command
    Signed-off-by: Adam Engebretson <adam@enge.me>

---
+++ src/Adamgoose/ForgeCli/Console/MonitoringCommand.php
@@ -0,0 +1,48 @@
+<?php namespace Adamgoose\ForgeCli\Console;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
+
+class MonitoringCommand extends Command {
+
+  /**
+   * Configure the command
+   */
+  protected function configure()
+  {
+    $this
+      ->setName('monitoring')
+      ->setDescription('Describe Monitoring Services configured by Forge');
+  }
+
+  /**
+   * @param InputInterface  $input
+   * @param OutputInterface $output
+   *
+   * @return void
+   */
+  protected function execute(InputInterface $input, OutputInterface $output)
+  {
+    try
+    {
+      $config = file_get_contents('/etc/php5/fpm/conf.d/newrelic.ini');
+    } catch(\Exception $e)
+    {
+      $output->writeln('<comment>NewRelic not installed.</comment>');
+      die();
+    }
+
+    $output->writeln('<info>=== NewRelic Config ===');
+
+    if(preg_match('/newrelic.license = "(.+)"/', $config, $results))
+    {
+      $output->writeln('<comment>Licence: ' . $results[1] . '</comment>');
+    }
+    else
+      $output->writeln('<comment>Cannot determine NewRelic License</comment>');
+
+    $output->writeln('<info>=== End of Output ===</info>');
+  }
+}
