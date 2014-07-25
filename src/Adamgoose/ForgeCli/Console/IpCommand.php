54d016ac368eb23e374883ca118cfb6e3d52924e
Adam Engebretson <adam@enge.me>
2014-07-25 16:25:09 -0500

    Adding IP Command
    Signed-off-by: Adam Engebretson <adam@enge.me>

---
+++ src/Adamgoose/ForgeCli/Console/IpCommand.php
@@ -0,0 +1,39 @@
+<?php namespace Adamgoose\ForgeCli\Console;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class IpCommand extends Command {
+
+  /**
+   * Configure the command
+   */
+  protected function configure()
+  {
+    $this
+      ->setName('ip')
+      ->setDescription('Get the public and private IPs for this server');
+  }
+
+  /**
+   * @param InputInterface  $input
+   * @param OutputInterface $output
+   *
+   * @return void
+   */
+  protected function execute(InputInterface $input, OutputInterface $output)
+  {
+    exec('ifconfig', $results);
+
+    $ifconfig = implode("\n", $results);
+    $regex = '/eth[0-1][\sA-z:0-9]+inet addr:([0-9.]+)/';
+    preg_match_all($regex, $ifconfig, $ips);
+
+    $output->writeln('<info>=== Begin IPs ===</info>');
+    $output->writeln('<comment>Public IP:  '.$ips[1][0].'</comment>');
+    $output->writeln('<comment>Private IP: '.$ips[1][1].'</comment>');
+    $output->writeln('<info>=== End of Output ===</info>');
+  }
+}
--- src/Adamgoose/ForgeCli/Console/MonitoringCommand.php
+++ src/Adamgoose/ForgeCli/Console/MonitoringCommand.php
@@ -3,7 +3,6 @@
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Finder\Finder;
 
 class MonitoringCommand extends Command {
 
