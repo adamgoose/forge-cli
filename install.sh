2b41eda1083c6bf6b14f009b4a317406e2740d34
Ángel Guzmán Maeso <shakaran@users.noreply.github.com>
2016-01-27 02:17:06 +0100

    Show a warning to user if it is not root

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+if [ `/usr/bin/id -u` -ne 0 ]; then
+    echo 'Please, run this script as root'
+    exit 1
+fi
+
 wget https://raw.githubusercontent.com/adamgoose/forge-cli/master/dist/forge-cli.phar
 sudo mv forge-cli.phar /usr/local/bin/forge
 sudo chmod +x /usr/local/bin/forge
